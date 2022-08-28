b03678664e0964de7a8b646707696a7f11716760
pengjingmin <pengjingmin@hortorgames.com>
2022-08-28 13:43:29 +0800

    feat: 增加article模板

---
+++ model.article.go
@@ -0,0 +1,29 @@
+package main
+
+import "errors"
+
+type article struct {
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
+var articleList = []article{
+	{ID: 1, Title: "Article 1", Content: "Article 1 body"},
+	{ID: 1, Title: "Article 1", Content: "Article 1 body"},
+}
+
+// Return a list of all the articles
+func getAllArticles() []article {
+	return articleList
+}
+
+func getArticleByID(id int) (*article, error) {
+	for _, a := range articleList {
+		if a.ID == id {
+			return &a, nil
+		}
+	}
+
+	return nil, errors.New("Article not found")
+}
